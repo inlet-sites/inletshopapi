8996dea2c4a45e3a701f0bbd21470ed6529af1ac
Lee Morgan <lee@leemorgan.dev>
2025-10-09 17:13:53 -0400

    Create all tests for the create password route.

--- src/tests/vendor/create_password.rs
+++ src/tests/vendor/create_password.rs
@@ -18,3 +18,58 @@ fn rejects_mismatched_passwords() {
     let result = create_password::handle_create_password(&v, i, t);
     assert!(matches!(result, Err(AppError::InvalidInput(_))));
 }
+
+#[test]
+fn rejects_short_password() {
+    let t = Uuid::new_v4().to_string();
+    let v = create_vendor(false, Some(t.clone()));
+    let i = CreatePasswordInput {
+        password: String::from("password"),
+        confirm_password: String::from("password")
+    };
+
+    let result = create_password::handle_create_password(&v, i, t);
+    assert!(matches!(result, Err(AppError::InvalidInput(_))));
+}
+
+#[test]
+fn rejects_invalid_token() {
+    let v = create_vendor(false, None);
+    let i = CreatePasswordInput {
+        password: String::from("password123"),
+        confirm_password: String::from("password123")
+    };
+
+    let result = create_password::handle_create_password(&v, i, String::from("abc"));
+    assert!(matches!(result, Err(AppError::Auth)));
+}
+
+#[test]
+fn rejects_password_exists() {
+    let t = Uuid::new_v4().to_string();
+    let v = create_vendor(true, Some(t.clone()));
+    let i = CreatePasswordInput {
+        password: String::from("password123"),
+        confirm_password: String::from("password123")
+    };
+
+    let result = create_password::handle_create_password(&v, i, t);
+    assert!(matches!(result, Err(AppError::Forbidden(_))));
+}
+
+#[test]
+fn returns_valid_document() {
+    let t = Uuid::new_v4().to_string();
+    let p = String::from("password123");
+    let v = create_vendor(false, Some(t.clone()));
+    let i = CreatePasswordInput {
+        password: p.clone(),
+        confirm_password: p.clone()
+    };
+
+    let result = create_password::handle_create_password(&v, i, t.clone()).unwrap();
+    assert!(result.contains_key("token"));
+    assert!(result.contains_key("pass_hash"));
+    assert_ne!(result.get("token").unwrap().to_string(), t);
+    assert_ne!(result.get("pass_hash").unwrap().to_string(), p);
+}
