3d196e9f04cb2418106ed197175b4d44b4155bdb
Lee Morgan <lee@leemorgan.dev>
2025-10-11 15:05:27 -0400

    Create tests and update docs for the get all vendors route.

--- src/controllers/vendor/get_all.rs
+++ src/controllers/vendor/get_all.rs
@@ -11,6 +11,8 @@ use crate::{
     models::vendor::Vendor,
     app_error::AppError
 };
+#[cfg(test)]
+use mongodb::bson::oid::ObjectId;
 
 #[derive(Serialize, Deserialize)]
 struct ResponseVendor {
@@ -76,4 +78,81 @@ fn create_response(vendors: Vec<Document>) -> Vec<ResponseVendor> {
             }
         }
     }).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    //create_projection_document
+    #[test]
+    fn correct_projection_data() {
+        let result = create_projection_document();
+
+        assert!(result.contains_key("_id"));
+        assert!(result.contains_key("store"));
+        assert!(result.contains_key("url"));
+        assert!(result.contains_key("public_data.slogan"));
+        assert!(result.contains_key("public_data.image"));
+        assert!(result.len() == 5);
+    }
+
+    //create_response
+    fn create_test_documents(public_data: bool) -> Vec<Document> {
+        let mut docs = Vec::new();
+
+        for _i in 0..10 {
+            let mut new_doc = doc!{
+                "_id": ObjectId::new(),
+                "store": "Example Store",
+                "url": "example-store",
+            };
+
+            if public_data {
+                new_doc.insert("public_data", doc!{
+                    "slogan": "Some slogan here",
+                    "image": "/image/12345"
+                });
+            }
+
+            docs.push(new_doc);
+        }
+
+        docs
+    }
+
+    #[test]
+    fn maps_fields_correctly() {
+        let docs = create_test_documents(false);
+        let docs_len = docs.len();
+        let docs_value = docs[1].get_object_id("_id").unwrap().to_hex();
+        let result = create_response(docs);
+
+        assert_eq!(result.len(), docs_len);
+        assert_eq!(result[0].store, "Example Store");
+        assert_eq!(result[1].id, docs_value);
+    }
+
+    #[test]
+    fn creates_public_data() {
+        let docs = create_test_documents(true);
+        let value_one = docs[1]
+            .get_document("public_data").unwrap()
+            .get_str("slogan").unwrap()
+            .to_string();
+        let value_two = docs[3]
+            .get_document("public_data").unwrap()
+            .get_str("image").unwrap()
+            .to_string();
+        let result = create_response(docs);
+
+        assert_eq!(
+            result[1].public_data.as_ref().unwrap().slogan.as_ref().unwrap(),
+            &value_one
+        );
+        assert_eq!(
+            result[3].public_data.as_ref().unwrap().image.as_ref().unwrap(),
+            &value_two
+        );
+    }
 }
