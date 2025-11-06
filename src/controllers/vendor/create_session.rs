9586d9b6d23654bb66e38026d8ce869709de5eb4
Lee Morgan <lee@leemorgan.dev>
2025-11-06 11:28:38 -0500

    Create route for retrieving client secret from Stripe for vendor onboarding.

--- src/controllers/vendor/create_session.rs
+++ src/controllers/vendor/create_session.rs
@@ -1,17 +1,50 @@
 use actix_web::{HttpResponse, HttpRequest, web, post};
 use mongodb::Database;
 use reqwest::Client;
+use serde_json::{Value, json};
+use std::collections::HashMap;
 use crate::{app_error::AppError, auth::vendor_auth};
 
 #[post("/vendor/connect/session")]
 pub async fn route(
     db: web::Data<Database>,
-    body: web::Json<Body>,
     req: HttpRequest
 ) -> Result<HttpResponse, AppError> {
     let vendor = vendor_auth(&db, &req).await?;
 
-    let client_secret = create_account_session(vendor.stripe.account_id);
+    if let Some(stripe) = vendor.stripe {
+        let client_secret = create_account_session(stripe.account_id).await?;
+        Ok(HttpResponse::Ok().json(json!({"client_secret": client_secret})))
+    } else {
+        Err(AppError::invalid_input("No Stripe account for this vendor"))
+    }
+}
 
-    Ok(HttpResponse::Ok().json())
+async fn create_account_session(id: String) -> Result<String, AppError> {
+    let stripe_secret = std::env::var("STRIPE_INLETSITES_KEY")
+        .map_err(|_| AppError::InternalError)?;
+
+    let mut params = HashMap::new();
+    params.insert("account", id.as_str());
+    params.insert("components[account_onboarding][enabled]", "true");
+
+    let client = Client::new();
+    let response = client
+        .post("https://api.stripe.com/v1/account_sessions")
+        .bearer_auth(stripe_secret)
+        .form(&params)
+        .send()
+        .await?;
+
+    match response.status().is_success() {
+        true => {
+            let data: Value = response.json().await?;
+            let client_secret = data["client_secret"]
+                .as_str()
+                .ok_or(AppError::StripeError)?
+                .to_string();
+            Ok(client_secret)
+        },
+        false => Err(AppError::StripeError)
+    }
 }
