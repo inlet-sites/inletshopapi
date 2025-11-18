1169900003b382a8b8cdb89db5aa62b3c8c48dfa
Lee Morgan <lee@leemorgan.dev>
2025-11-18 14:55:10 -0500

    Create documentation for a vendor to retrieve a single product of their own.

--- src/routes/vendor.rs
+++ src/routes/vendor.rs
@@ -31,4 +31,5 @@ pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(products::create::route);
     cfg.service(products::delete::route);
     cfg.service(products::get_many::route);
+    fcg.service(products::get_one::route);
 }
